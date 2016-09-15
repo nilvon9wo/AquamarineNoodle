be47a6289381e7c5648b6cd901caa03acb825c2f
Brian <kessler.bm@gmail.com>
2016-09-15 09:51:05 +0200

    fixed logging

--- src/registration/RegisterViewModel.ts
+++ src/registration/RegisterViewModel.ts
@@ -7,24 +7,24 @@ import ViewInterface from '../common/ViewInterface';
 class RegisterViewModel implements ViewInterface {
 
     constructor($scope: RegisterViewInterface, $http: ng.IHttpService, private $logger: LoggerInterface) {
-        $scope.save = function () {
-        $logger.log('Saving...');
-        $http.post('/api/register', {
-            age: $scope.age,
-            name: $scope.name,
-            salutation: $scope.salutation
-        },
-            { headers: { 'Content-Type': 'application/json' } }
-        )
-            .success(_ => {
-                alert('You are registered!');
-                $logger.log('Saving Success.');
-            })
-            .error(_ => {
-                alert('Sorry, not possible!');
-                $logger.log('Saving Failed.');
-            });
-    };
+        $scope.save = function() {
+            $logger.log('Saving...');
+            $http.post('/api/register', {
+                age: $scope.age,
+                name: $scope.name,
+                salutation: $scope.salutation
+            },
+                { headers: { 'Content-Type': 'application/json' } }
+            )
+                .success(_ => {
+                    alert('You are registered!');
+                    $logger.log('Saving Success.');
+                })
+                .error(_ => {
+                    alert('Sorry, not possible!');
+                    $logger.log('Saving Failed.');
+                });
+        };
     }
 }
 
