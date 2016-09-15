270c4266683f5da1830b57aa495bc78d36511d0d
Brian <kessler.bm@gmail.com>
2016-09-15 21:56:23 +0200

    renamed UI controllers to reflect this being what they are.

--- src/common/UIControllerAbstract.ts
+++
@@ -1,8 +0,0 @@
-import UIControllerInterface from './UIControllerInterface';
-
-abstract class UIControllerAbstract implements UIControllerInterface {
-    constructor(public app: any) {
-    }
-}
-
-export default UIControllerAbstract;
--- src/common/ViewInterface.ts
+++
@@ -1,4 +0,0 @@
-interface ViewInterface {
-}
-
-export default ViewInterface;
